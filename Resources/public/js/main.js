be36ad188bd6883791827d353edecf95c1d46d07
Mateus Guerra <mateus.guerra@a2c.com.br>
2016-05-13 09:46:12 -0300

    modal delete batch

--- Resources/public/js/main.js
+++ Resources/public/js/main.js
@@ -1,5 +1,54 @@
 jQuery(document).ready(function($) {
     
+    $('.icheck').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue'
+    });
+
+    $('select.select2').select2({
+        width : '100%'
+    });
+
+    $('select.select2').on('change', function () {
+        $(this).valid();
+    });
+
+    $('#checkbox-select-all').on('ifChecked', function(event){
+        $('.checkbox-select').iCheck('check');
+    });
+
+    $('#checkbox-select-all').on('ifUnchecked', function(event){
+        $('.checkbox-select').iCheck('uncheck');
+    });
+
+    $('.checkbox-select').on('ifUnchecked', function(event){
+        $('#checkbox-select-all').iCheck('indeterminate');
+    });
+    
+    var saidOk = false;
+
+    $('#batch-list').on('submit', function(event){
+        if($('#batch-actions').val() == 'deleteBatch' && $(".checkbox-select:checked").length > 0) {
+            if(saidOk) {
+                $('input[name=_method]').val('POST');
+            } else {
+                $('#modalDeleteBatch').modal({
+                    'show': true
+                });
+
+                return false;
+            }
+            
+        } else {
+            $('input[name=_method]').val('POST');
+        }
+    });
+
+    $('#modalDeleteBatch').find('button[type=submit]').on('click', function(){
+        saidOk = true;
+        $('#batch-list').closest('form').submit();
+    });
+
     $.validator.setDefaults({
         errorElement: "span",
         errorClass: "help-block",
@@ -31,19 +80,6 @@ jQuery(document).ready(function($) {
                 }
             }
         });
-    });
-
-    $('.icheck').iCheck({
-        checkboxClass: 'icheckbox_square-blue',
-        radioClass: 'iradio_square-blue'
-    });
-
-    $('select.select2').select2({
-        width : '100%'
-    });
-
-    $('select.select2').on('change', function () {
-        $(this).valid();
     });
 
 });
